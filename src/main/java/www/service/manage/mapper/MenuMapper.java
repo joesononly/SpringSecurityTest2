310a8ac0d48cab97fc82b13ff9a955dc57f2fb2e
joeson <645768552@qq.com>
2019-05-19 20:56:46 +0800

    更新视图/职责/菜单相关DAO操作

--- src/main/java/www/service/manage/mapper/MenuMapper.java
+++ src/main/java/www/service/manage/mapper/MenuMapper.java
@@ -3,15 +3,7 @@ package www.service.manage.mapper;
 import www.bean.Menu;
 import www.service.BaseMapper;
 
-import java.util.List;
 
-public interface MenuMapper{
+public interface MenuMapper extends  BaseMapper<Menu>{
 
-    public void insert(Menu data);
-
-    public void delete(Menu data);
-
-    public List<Menu> select(Menu data);
-
-    public void update(Menu data);
 }
